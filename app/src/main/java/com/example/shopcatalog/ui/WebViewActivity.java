5c04c8dbc9a82a931b6ecf53c27943aa8c8eeb46
I-Prokopyuk <prok.pismo@gmail.com>
2021-02-02 17:23:52 +0600

    Added the Reactivenetwork library

--- app/src/main/java/com/example/shopcatalog/ui/WebViewActivity.java
+++ app/src/main/java/com/example/shopcatalog/ui/WebViewActivity.java
@@ -2,47 +2,65 @@ package com.example.shopcatalog.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shopcatalog.R;
-import com.example.shopcatalog.utils.OnlineConnectedStatus;
+import com.example.shopcatalog.common.Constants;
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork;
 
-import javax.inject.Inject;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
-import dagger.android.support.DaggerAppCompatActivity;
-import io.reactivex.Observable;
+public class WebViewActivity extends AppCompatActivity {
 
-public class WebViewActivity extends DaggerAppCompatActivity {
+    private Disposable internetDisposable;
+    WebView webView;
+    String url;
 
-    @Inject
-    OnlineConnectedStatus onlineConnectedStatus;
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        internetDisposable = ReactiveNetwork.observeInternetConnectivity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(isConnected -> {
+                    if (isConnected) {
+                        webView.setVisibility(View.VISIBLE);
+                        webView.clearCache(true);
+                        webView.loadUrl(url);
+                    } else {
+                        webView.setVisibility(View.INVISIBLE);
+                        Toast.makeText(this, R.string.warning_no_connection, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        Log.i("myLogs", "WebViewActivity Resume.....");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
 
-        Observable.just(onlineConnectedStatus.isOnlineConnected())
-                .subscribe(aBoolean -> Log.i("myLogs","Connection "+aBoolean));
+        webView = (WebView) findViewById(R.id.webView);
 
-//        WebView webView = (WebView) findViewById(R.id.webView);
-//
-//        if (onlineConnectedStatus.isOnlineConnected()) {
-//
-//            Bundle bundle = getIntent().getExtras();
-//
-//            String url;
-//
-//            if (bundle == null) {
-//                url = Constants.CATALOG_API_BASE_URL;
-//            } else url = bundle.getString("url");
-//
-//            webView.clearCache(true);
-//            webView.loadUrl(url);
-//
-//            Log.i("myLogs", url + " <<<<<<<< Full url");
-//        } else {
-//            webView.setVisibility(View.INVISIBLE);
-//            Snackbar.make(findViewById(R.id.parent_layout), R.string.warning_no_connection, Snackbar.LENGTH_INDEFINITE).show();
-//        }
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle == null) {
+            url = Constants.CATALOG_API_BASE_URL;
+        } else url = bundle.getString("url");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        internetDisposable.dispose();
+        Log.i("myLogs", "WebViewActivity pause.....");
     }
 }
